f6fca656ee3ac8a0d53327a431c136e85999e922
Sravya <sravyavissamsetty@gmail.com>
2020-11-04 18:38:53 +0530

    number of bits to be flipped to convert A to B
    https://www.geeksforgeeks.org/count-number-of-bits-to-be-flipped-to-convert-a-to-b/

---
+++ BitManipulation/ConvertAtoB.cpp
@@ -0,0 +1,57 @@
+/*
+
+You are given two numbers A and B. 
+Write a program to count number of bits needed to be flipped to convert A to B.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. 
+T testcases follow. The first line of each test case is A and B separated by a space.
+
+Output:
+For each testcase, in a new line, print the number of bits needed to be flipped.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ A, B ≤ 103
+
+Example:
+Input:
+1
+10 20
+Output:
+4
+
+Explanation:
+Testcase1:
+A  = 01010
+B  = 10100
+Number of bits need to flipped = 4
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int bitsToFlip(int a, int b)
+{
+    int temp = a^b;
+    int count =0;
+    while(temp)
+    {
+        temp = temp&(temp-1);
+        count++;
+    }
+    return count;
+}
+int main() 
+{
+    int t;
+    cin>>t;
+	int a,b;
+	while(t--)
+	{
+	cin>>a>>b;
+	cout<<bitsToFlip(a,b);
+	}
+	return 0;
+}
